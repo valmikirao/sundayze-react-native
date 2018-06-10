72690e90e34f9f8069f95bf7fe4daca11be5527c
Valmiki Rao <valmikirao@Valmikis-MacBook-Pro.local>
2018-06-10 10:14:54 -0400

    Working Stream POC is App.FeedPOS.tsx

--- react-native/App.js
+++ react-native/App.js
@@ -9,6 +9,14 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -19,23 +27,43 @@ var stream_for_client_1 = __importDefault(require("./lib/stream-for-client"));
 var App = /** @class */ (function (_super) {
     __extends(App, _super);
     function App(props) {
-        return _super.call(this, props) || this;
+        var _this = _super.call(this, props) || this;
+        _this.state = {
+            note: null,
+            shares: []
+        };
+        stream_for_client_1.default.listToronto(function (responseShares) {
+            var newShares = responseShares.map(function (share) { return ({
+                note: share.note,
+                time: share.time
+            }); });
+            _this.setState(__assign({}, _this.state, { shares: _this.state.shares.concat(newShares) }));
+        });
+        return _this;
     }
     App.prototype.render = function () {
         var _this = this;
+        var state = this.state;
         var onPress = function () {
-            var note = _this.state.note;
+            var note = state.note;
             stream_for_client_1.default.shareToronto({ note: note });
         };
+        debugger;
+        var key = 0;
+        var shares = state.shares;
+        var sharesAsComponents = shares.map(function (share) { return react_1.default.createElement(react_native_1.Text, { key: key++ },
+            share.note,
+            " at ",
+            share.time); });
         return (react_1.default.createElement(react_native_1.View, { style: styles.container },
+            sharesAsComponents,
             react_1.default.createElement(react_native_1.Text, null, "Write something to post"),
-            react_1.default.createElement(react_native_1.TextInput, { style: { height: 40, padding: 10 }, placeholder: "note", onChangeText: function (note) { return _this.setState({ note: note }); } }),
+            react_1.default.createElement(react_native_1.TextInput, { style: { height: 40, padding: 10 }, placeholder: "note", onChangeText: function (note) { return _this.setState(__assign({}, state, { note: note })); } }),
             react_1.default.createElement(react_native_1.Button, { onPress: onPress, title: "Share!" })));
     };
     return App;
 }(react_1.default.Component));
 exports.default = App;
-exports.default = ;
 var styles = react_native_1.StyleSheet.create({
     container: {
         flex: 1,
