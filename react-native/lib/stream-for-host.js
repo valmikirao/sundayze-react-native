24575382e69b187d1e800dc56864bb91e5bab80a
Valmiki Rao <valmikirao@Valmikis-MacBook-Pro.local>
2018-06-17 14:00:28 -0400

    rm react-native

--- react-native/lib/stream-for-host.js
+++
@@ -1,5 +0,0 @@
-const Stream = require('getstream');
-const client = Stream.connect('htk5qzfncgtm', 'ttwyqe38m34tqcgw27jz5a8parjpx7unwrp5c4pqzemp3ee6zkpqfev6x2m5tt3v', '36624');
-
-export const torontoFeed = client.feed('group', 'toronto-friends');
-
