fd1787ecdd1f3c52cfa866d0608dbac8f1735521
Valmiki Rao <valmikirao@Valmikis-MacBook-Pro.local>
2018-06-17 13:59:14 -0400

    Got non-expo app working in ios

--- ReactNativeClean/App.js
+++ ReactNativeClean/App.js
@@ -1,58 +1,105 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider }  from 'react-redux';
+import { StyleSheet, Text, TextInput, View, Button, AppRegistry, Image } from 'react-native';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+// import Stream from './lib/stream-for-client';
+import { GroupView } from './lib/components/group-view';
+import { styles } from './lib/styles/app-styles';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const image_x = require('./src-assets/X.png');
+const selfie = require('./src-assets/selfie.png');
+const selfie2 = require('./src-assets/selfie-2.png');
 
-type Props = {};
-export default class App extends Component<Props> {
+let DEBUG_SHARED_ITEMS = [
+  {
+    note: 'Hello',
+    time: '1/1/2018',
+    pic: selfie
+  },
+  {
+    note: 'Goodbye',
+    time: '1/1/2019',
+    pic: selfie2,
+  },
+  {
+    note: 'Good morning',
+    time: '1/2/2019',
+    pic: selfie,
+  },
+  {
+    note: 'Good evening',
+    time: '1/2/2019',
+    pic: selfie2,
+  }
+];
+
+// DEBUG_SHARED_ITEMS = [
+//   ...DEBUG_SHARED_ITEMS,
+//   ...DEBUG_SHARED_ITEMS,
+//   ...DEBUG_SHARED_ITEMS
+// ];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Stream.listToronto(responseShares => {
+    //     const newShares = responseShares.map(share => ({
+    //             note: share.note,
+    //             time: share.time
+    //     }));
+
+    //     this.setState({
+    //         ...this.state,
+    //         shares : [
+    //             ...this.state.shares,
+    //             ...newShares
+    //         ]
+    //     })
+    // })
+  }
+
   render() {
+    const {state, props} = this;
+    
+    const onPress = () => {
+      const {note} = state;
+
+      // Stream.shareToronto({note});
+    };
+
+    let key = 0;
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native! Let's edit stuff
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={styles.app }>
+        <GroupView/>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+function reducer(state, action) {
+  switch (action.type) {
+    case 'INIT' : {
+      return {
+        sharedItems : DEBUG_SHARED_ITEMS
+      }
+    }
+    default : 
+      return state;
+  }
+}
+
+let store = createStore(reducer);
+store.dispatch({type : 'INIT'});
+
+export default class ReduxApp extends React.Component {
+  render() {
+    return <Provider store={ store }>
+      <App/>
+    </Provider>;
+  }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => ReduxApp);
---
+++ ReactNativeClean/lib/components/group-view/group-view-styles.js
@@ -0,0 +1,19 @@
+
+export default  {
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sharedItem: {
+    image: {
+      width: 200,
+      height: 200
+    },
+    view: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  }
+};
