f626862513462f6ea0cd19e6c991210342864762
Valmiki Rao <valmikirao@Valmikis-MacBook-Pro.local>
2018-06-17 14:02:00 -0400

    Clean up some stuff I'm not using any more

--- get-stream/src/lib/stream-client-native.js
+++
@@ -1,6 +0,0 @@
-var stream = require('getstream');
-
-client = stream.connect('enpdtmyzrhhh', null, '36624');
-eric = client.feed('user', 'eric', 'AsNQVPXH6EO_CAKQUIAEQQIssw0');
-
-module.exports = {client, eric};
--- theme/data-only/views/user/auth/login.php
+++
@@ -1,5 +0,0 @@
-<?php use yii\helpers\Json ?>
-<?php use Yii?>
---
-_csrfParam: <?= Json::encode(Yii::$app->request->csrfParam) ?>
-_csrfToken: <?= Json::encode(Yii::$app->request->csrfToken) ?>
